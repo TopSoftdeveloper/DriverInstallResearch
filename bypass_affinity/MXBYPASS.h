d5006ab80f79d055a733d255d3f1d5097095e032
topsoftdeveloper <topsoftdeveloper@outlook.com>
2026-02-23 12:32:16 +0100

    bypass tcp table

--- bypass_affinity/MXBYPASS.h
+++ bypass_affinity/MXBYPASS.h
@@ -4,29 +4,35 @@
 // that uses this DLL. This way any other project whose source files include this file see
 // MXBYPASS_API functions as being imported from a DLL, whereas this DLL sees symbols
 // defined with this macro as being exported.
+
 #ifdef MXBYPASS_EXPORTS
-#define MXBYPASS_API __declspec(dllexport)
+#define MXBYPASS_API extern "C" __declspec(dllexport)
 #else
-#define MXBYPASS_API __declspec(dllimport)
+#define MXBYPASS_API extern "C" __declspec(dllimport)
 #endif
 #pragma comment(lib,"MXBYPASS.lib")
 
-#ifdef __cplusplus
-extern "C" {
-#endif
+#include <Windows.h>
+#include <vector>
+#include <string>
 
-MXBYPASS_API void init_map_driver(void);
+
+MXBYPASS_API void init_map_driver();
 MXBYPASS_API bool is_tool_ready(void);
 MXBYPASS_API bool tool_push_affinity_rule(const CHAR* process_name, INT set_dwAffinity, INT get_dwAffinity);
-MXBYPASS_API bool tool_pop_affinity_rule(void);
-MXBYPASS_API int tool_get_affinity_rules_cnt(void);
-MXBYPASS_API bool tool_clear_affinity_rules(void);
+MXBYPASS_API bool tool_pop_affinity_rule();
+MXBYPASS_API int tool_get_affinity_rules_cnt();
+MXBYPASS_API bool tool_clear_affinity_rules();
 MXBYPASS_API int tool_get_process_pid(LPCWSTR name);
 MXBYPASS_API bool tool_push_prochide_rule(INT pid);
-MXBYPASS_API bool tool_pop_prochide_rule(void);
-MXBYPASS_API int tool_get_prochide_rules_cnt(void);
-MXBYPASS_API bool tool_clear_prochide_rules(void);
+MXBYPASS_API bool tool_pop_prochide_rule();
+MXBYPASS_API int tool_get_prochide_rules_cnt();
+MXBYPASS_API bool tool_clear_prochide_rules();
 
-#ifdef __cplusplus
-}
-#endif
+
+
+MXBYPASS_API void get_desktops_list(std::vector<std::wstring>& outVector);
+MXBYPASS_API void init_drawing_thread(const wchar_t* desktop_name);
+MXBYPASS_API void tool_push_draw_command(int x, int y, int size, int timeout, float r, float g, float b, float a, const wchar_t* message, bool have_bg = false, float r2 = 255, float g2 = 255, float b2 = 255, float a2 = 255);
+MXBYPASS_API void tool_pop_last_draw_command();
+MXBYPASS_API void tool_clear_draw_commands();
--- bypass_affinity/bypass_affinity.cpp
+++ bypass_affinity/bypass_affinity.cpp
@@ -1,8 +1,10 @@
-
+﻿
 #include <iostream>
 #include <windows.h>
+#include <cstdlib>
 
 #include "MXBYPASS.h"
+
 
 
 int main()
@@ -15,11 +17,37 @@ int main()
 	{
 		printf("faild to load the driver ,wtf?\n");
 		exit(0);
-
 	}
 
 	tool_clear_affinity_rules();
 	tool_clear_prochide_rules();
+	tool_clear_draw_commands();
+
+	std::vector<std::wstring> outVector;
+	get_desktops_list(outVector);
+	for (int i = 0; i < outVector.size(); i++)
+	{
+		printf("desktop:%ws\n", outVector[i].c_str());
+		if (!wcscmp(outVector[i].c_str(), L"Default"))
+		{
+			init_drawing_thread(outVector[i].c_str());
+		}
+	}
+
+	//Sleep(1000);
+	//init_drawing_thread(L"MyExplorerDesktop");
+
+	int size = 20;
+	tool_push_draw_command(100, 100, size, 5000, 255, 0, 0, 255, L"hello world 1 !", false);
+	tool_push_draw_command(100, 100, size, 4000, 255, 0, 0, 255, L"hello world 2 !", false);
+	tool_push_draw_command(100, 100, size, 3000, 255, 0, 0, 255, L"hello world 3 !", true);
+	tool_push_draw_command(100, 100, size, 2000, 255, 0, 0, 255, L"hello world 4 !", true, 0, 0, 255, 255);
+	tool_push_draw_command(100, 100, size, 1000, 255, 0, 0, 255, L"hello world 5 !", true,0, 255,0, 255);
+	tool_push_draw_command(100, 100, size, 10000, 255, 0, 0, 255, L"你好世界ا", true, 255, 255,0, 255);
+	tool_push_draw_command(100, 100, size, 99999999, 255, 0, 0, 255, L"مرحبا ايها العالم", true,255,255,255,255);
+
+
+	printf("tool_push_affinity_rule\n ");
 
 	if (!tool_push_affinity_rule("wda_monitor.exe", WDA_NONE, WDA_MONITOR))
 	{
@@ -27,26 +55,18 @@ int main()
 		exit(0);
 	}
 
-	printf("waiting for wda_monitor.exe to get pid\n");
+	printf("tool_push_prochide_rule GetCurrentProcessId\n ");
 
-	while (true)
+	if (!tool_push_prochide_rule(GetCurrentProcessId()))
 	{
-		int pid = tool_get_process_pid(L"wda_monitor.exe");
-		if (pid != 0)
-		{
-			if (!tool_push_prochide_rule(pid))
-			{
-				printf("faild to add PROCHIDE rule !");
-				exit(0);
-			}
-			printf("wda_monitor pid:%d hided ok\n", pid);
-			break;
-		}
-		Sleep(500);
+		printf("faild to add PROCHIDE rule !");
+		exit(0);
 	}
+
 	printf("all good !\n");
 	getchar();
 	getchar();
+	
 
 	return 0;
 }
